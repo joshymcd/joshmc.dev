335ed0a27094edcfaa73e284b3aa3b0512eeed1d
Joshua McDonald <joshymcd92@gmail.com>
2025-05-04 11:48:50 +0100

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import aws from "astro-sst";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://joshmc.dev",
   output: "server",
   adapter: aws(),
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
